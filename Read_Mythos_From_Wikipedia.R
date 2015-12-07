0feca939db7ef000dfb06794ed587ce07b9ce6ce
julianhatwell <julian.hatwell@googlemail.com>
2015-12-07 17:11:00 +0800

    some refactoring especially of subs

--- Read_Mythos_From_Wikipedia.R
+++ Read_Mythos_From_Wikipedia.R
@@ -1,60 +1,51 @@
-require(RCurl)
-require(XML)
-require(dplyr)
+library(RCurl)
+library(XML)
+library(dplyr)
 
 # function for cleaning up vestiges of HTML origins
-cleanText <- function(HTMLTbl) {
-     names(HTMLTbl) <- gsub("\\n", " " , names(HTMLTbl))
-     for (i in names(HTMLTbl)) {
-     HTMLTbl[[i]] <- gsub("\\n", " " , HTMLTbl[[i]])
-     HTMLTbl[[i]] <- gsub("\\[[0-9]+\\]", "", HTMLTbl[[i]])
-     HTMLTbl[[i]] <- gsub(",[A-T]", " ", HTMLTbl[[i]])
-  }
-  HTMLTbl
+cleanText <- function(char.vec) {
+  clean.vec <- gsub("\\n", " " , char.vec)
+  clean.vec <- gsub("\\[[0-9]+\\]", "", clean.vec)
+  clean.vec <- gsub(",[A-T]", " ", clean.vec)
+  clean.vec <- gsub("\\^ ", "", clean.vec)
+  clean.vec
 }
 
-# get Great Old One content from Wikipedia
+# get mythos content from Wikipedia
 GOOUrl <- getURL("https://en.wikipedia.org/wiki/Cthulhu_Mythos_deities#Great_Old_Ones")
-GOO <- readHTMLTable(GOOUrl, which = 1, header = TRUE, trim = TRUE, stringsAsFactors = FALSE)
-GOO <- cleanText(GOO)
-
-# get Other God and misc content from Wikipedia
 OGUrl <- getURL("https://en.wikipedia.org/wiki/Cthulhu_Mythos_deities#List")
+
+# read Great Old One content HTML tree
+GOO <- readHTMLTable(GOOUrl, which = 1, header = TRUE, trim = TRUE, stringsAsFactors = FALSE)
+# clean up
+names(GOO) <- cleanText(names(GOO))
+GOO <- sapply(GOO, cleanText)
+
+# read Other God content from HTML tree
 OuterG <- readHTMLList(OGUrl, trim = TRUE, which = 3)
 ElderG <- readHTMLList(OGUrl, trim = TRUE, which = 4)
-Refs <- readHTMLList(OGUrl, trim = TRUE, which = 11)
-Bibl <- readHTMLList(OGUrl, trim = TRUE, which = 12)
-Places <- readHTMLList(OGUrl, trim = TRUE, which = 15)
-Characs <- readHTMLList(OGUrl, trim = TRUE, which = 16)
-Deities <- readHTMLList(OGUrl, trim = TRUE, which = 17)
-Species <- readHTMLList(OGUrl, trim = TRUE, which = 18)
-
-
 OtherG <- data.frame(import =  c(OuterG, ElderG)
                      , category = c(rep("Outer", times = length(OuterG))
                                     , rep("Elder", times = length(ElderG))
                                     )
                     )
 
-# try using tidyr to replace this sub
+# Split the Wiki page indexing from the name
 Name <- sub("\\d.\\d{1,2} ", "", OtherG$import)
 WikiIndex <- trimws(mapply(FUN = sub, pattern = Name, replacement = "", x = OtherG$import))
 names(WikiIndex) <- NULL
-
 OtherG <- mutate(OtherG, name = Name, wikiIndex = WikiIndex) %>%
   select(-import)
-
 rm(list=(c("Name", "WikiIndex")))
 
-# More content
+# More OG content
 OGHTML <- htmlParse(OGUrl)
-
 OG1 <- xpathApply(OGHTML, "//table/following-sibling::p", xmlValue)
-MiscText <- unlist(OG1[1])
 
 # This has all the lists and semi structured information
 OGText = unlist(OG1[4:(length(OG1)-1)])
 
+# The following steps ensure the text blocks match up with the OG listing
 OuterGParagraph <- paste(OGText[38]
                      , OGText[39]
                      , sep = "\n ")
@@ -65,20 +56,40 @@ OuterGCatalog <- c(OGText[1:37]
                        , OGText[41]
                        , sep = "\n ")
                , OGText[42])
-
 ElderGParagraph <- paste(OGText[43]
                              , OGText[44]
                              , OGText[45]
                              , sep = "\n ")
-
 ElderGCatalog <- c(OGText[46:55])
 
 OtherG$Description <- c(OuterGCatalog, ElderGCatalog)
 
+# clean up
+OtherG <- sapply(OtherG, cleanText)
 
 GOnesText <- OG1[[length(OG1)]]
 # There is a Great Ones table that I want to extract.
 # Seems a bit tricky
+
+# read misc content from HtML tree
+Refs <- readHTMLList(OGUrl, trim = TRUE, which = 11)
+Bibl <- readHTMLList(OGUrl, trim = TRUE, which = 12)
+Placs <- readHTMLList(OGUrl, trim = TRUE, which = 15)
+Characs <- readHTMLList(OGUrl, trim = TRUE, which = 16)
+Deits <- readHTMLList(OGUrl, trim = TRUE, which = 17)
+Specs <- readHTMLList(OGUrl, trim = TRUE, which = 18)
+MiscText <- unlist(OG1[1])
+# clean up
+Mythos.Misc <- list(References = Refs
+                    , Bibliography = Bibl
+                    , Places = Placs
+                    , Characters = Characs
+                    , Deities = Deits
+                    , Species = Specs
+                    , Miscellaneous = MiscText)
+# cleanup
+Mythos.Misc <- lapply(Mythos.Misc, cleanText)
+
 
 # word counting function
 findWords <- function(t) {
