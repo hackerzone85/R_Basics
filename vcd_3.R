708bf7ba9dfb18413dec2908422e7fccef49bb0a
julianhatwell <julian.hatwell@googlemail.com>
2016-03-14 08:11:25 +0800

    common distributions

--- vcd_3.R
+++ vcd_3.R
@@ -1,8 +1,18 @@
 library(vcd)
 # distributions
+set.seed(12345)
 k <- 0:12
 Pk <- dbinom(k, 12, 1/3)
 b <- barplot(Pk, names.arg = k
              , xlab = "number of successes"
              , ylab = "probability")
 lines(x = b, y = Pk, col = "red")
+
+Weldon_df <- as.data.frame(WeldonDice)
+# collapse last three cols of simulated set
+Pk <- c(Pk[1:10], sum(Pk[11:13]))
+Exp <- round(26306 * Pk, 5) # expected
+Diff <- Weldon_df$Freq - Exp
+Chisq <- Diff^2/Exp
+data.frame(Weldon_df, prob = round(Pk, 5), Exp, Diff, Chisq)
+
